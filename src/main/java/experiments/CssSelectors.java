e3d46a8f4ad17f399ef479ee3302394e35555aeb
MedSveta <sveta1978medved@gmail.com>
2025-10-03 11:58:28 +0300

    CssSelectors2

--- src/main/java/experiments/CssSelectors.java
+++ src/main/java/experiments/CssSelectors.java
@@ -1,6 +1,7 @@
 package experiments;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -35,15 +36,57 @@ public class CssSelectors {
         radioButton.click();
         pause(4000);
         //driver.navigate().back();
+        hideBanner();
+        hideFooter();
         WebElement btnYes = driver.findElement(By
                 .cssSelector("label[for='yesRadio']"));
         btnYes.click();
-
         pause(4000);
+
+        driver.navigate().back();
+        driver.navigate().back();
+        hideFooter();
+        WebElement divBookStore = driver.findElement(By.cssSelector("div[class='category-cards'] " +
+                "div[class='card mt-4 top-card']:last-child"));
+        divBookStore.click();
+        pause(4000);
+        driver.navigate().back();
+        pause(4000);
+        WebElement divWidgets = driver.findElement(By.cssSelector("div[class='category-cards'] " +
+                "div[class='card mt-4 top-card']:nth-child(4)"));
+        divWidgets.click();
+        pause(4000);
+        driver.navigate().back();
+        pause(4000);
+        WebElement divAlerts = driver.findElement(By.cssSelector("div[class='category-cards'] " +
+                "div[class='card mt-4 top-card']:nth-child(3) div[class='card-body'] h5"));
+        System.out.println(divAlerts.getText());
+
         //driver.close();
         driver.quit();
-
     }
+
+    @Test
+    public void clickCheckBox(){
+        driver.get("https://demoqa.com/");
+        driver.manage().window().maximize();
+        WebElement divElements = driver.findElement(By.cssSelector(".card"));
+        //WebElement divElements = driver.findElement(By.cssSelector("div[class='card mt-4 top-card']"));
+        divElements.click();
+        WebElement btnCheckBox = driver.findElement(By.id("item-1"));
+        //WebElement btnCheckBox = driver.findElement(By.cssSelector("#item-1"));
+        //WebElement btnCheckBox = driver.findElement(By.cssSelector("li[id='item-1']"));
+        //WebElement btnCheckBox = driver.findElement(By.cssSelector("*[id='item-1']"));
+        btnCheckBox.click();
+        hideBanner();
+        hideFooter();
+        WebElement checkBox = driver.findElement(By
+                .cssSelector("svg[class='rct-icon rct-icon-uncheck']"));
+        checkBox.click();
+        pause(4000);
+        driver.quit();
+    }
+
 
     public void pause(int time){
         try {
@@ -51,5 +94,15 @@ public class CssSelectors {
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    public void hideBanner(){
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        js.executeScript("document.querySelector('#fixedban').style.display='none'");
+    }
+
+    public void hideFooter(){
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        js.executeScript("document.querySelector('footer').style.display='none'");
     }
 }

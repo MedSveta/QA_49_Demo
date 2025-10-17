9fd1ef95f68773dab9791ea21f7382ef1ab6343a
MedSveta <sveta1978medved@gmail.com>
2025-10-17 11:49:21 +0300

    Start POM

---
+++ src/test/java/tests/PracticeFormTests.java
@@ -0,0 +1,11 @@
+package tests;
+
+import manager.AppManager;
+import org.testng.annotations.Test;
+
+public class PracticeFormTests extends AppManager {
+    @Test
+    public void practiceFormPositiveTest(){
+
+    }
+}
